ad90387359bd498aadd9f4e30646f3f22b378541
olasunmade babafemi <babafemiolasunmade@gmail.com>
2024-11-29 12:40:21 +0100

    .

--- src/middleware/authenticated.middleware.ts
+++ src/middleware/authenticated.middleware.ts
@@ -6,54 +6,57 @@ import { Request, Response, NextFunction } from "express";
 import jwt, { verify } from "jsonwebtoken";
 import axios from "axios";
 
-
 async function authenticatedMiddleware(
-  req: Request,
-  res: Response,
-  next: NextFunction
+	req: Request,
+	res: Response,
+	next: NextFunction
 ): Promise<Response | void> {
-  try {
-    const bearer = req.headers.authorization || "Bearer abcdef";
-    console.log("🚀 ~ bearer:", bearer);
+	try {
+		const bearer = req.headers.authorization || "Bearer abcdef";
+		console.log("🚀 ~ bearer:", bearer);
 
-    if (!bearer || !bearer.startsWith("Bearer ")) {
-      if (!req.path.includes('/product/fetch')) {
-        return next(new HttpException(401, "Unauthorized"));
-      }
-      return next(); // Allow through for `/product/fetch`
-    }
+		if (!bearer || !bearer.startsWith("Bearer ")) {
+			if (
+				!req.path.includes("/product/fetch") &&
+				!req.path.includes("/order/create-custom")
+			) {
+				return next(new HttpException(401, "Unauthorized"));
+			}
+			return next(); // Allow through for `/product/fetch`
+		}
 
-    const accessToken = bearer.split("Bearer ")[1].trim();
-    const productPath = req.path.includes('/product/fetch');
-    console.log("🚀 ~ productPath:", productPath);
+		const accessToken = bearer.split("Bearer ")[1].trim();
+		const productPath = req.path.includes("/product/fetch");
+		console.log("🚀 ~ productPath:", productPath);
 
-    const payload = await verifyToken(accessToken);
-    if (payload instanceof jwt.JsonWebTokenError) {
-      if (!productPath) {
-        return next(new HttpException(401, "Unauthorized"));
-      }
-      return next(); // Allow through for `/product/fetch`
-    }
+		const payload = await verifyToken(accessToken);
+		if (payload instanceof jwt.JsonWebTokenError) {
+			if (!productPath) {
+				return next(new HttpException(401, "Unauthorized"));
+			}
+			return next(); // Allow through for `/product/fetch`
+		}
 
-    const user = await userModel.findById(payload.id)
-      .populate('business')
-      .populate('wallet')
-      .populate('cart')
-      .populate('card');
+		const user = await userModel
+			.findById(payload.id)
+			.populate("business")
+			.populate("wallet")
+			.populate("cart")
+			.populate("card");
 
-    if (!user && !productPath) {
-      return next(new HttpException(401, "Unauthorized"));
-    }
+		if (!user && !productPath) {
+			return next(new HttpException(401, "Unauthorized"));
+		}
 
-    req.user = user;
-    next();
-  } catch (error) {
-    console.log("🚀authenticatedMiddleware ~ error:", error);
-    if (!req.path.includes('/product/fetch')) {
-      return next(new HttpException(401, "Unauthorized"));
-    }
-    next(); // Allow through for `/product/fetch` on error
-  }
+		req.user = user;
+		next();
+	} catch (error) {
+		console.log("🚀authenticatedMiddleware ~ error:", error);
+		if (!req.path.includes("/product/fetch")) {
+			return next(new HttpException(401, "Unauthorized"));
+		}
+		next(); // Allow through for `/product/fetch` on error
+	}
 }
 
 export default authenticatedMiddleware;
